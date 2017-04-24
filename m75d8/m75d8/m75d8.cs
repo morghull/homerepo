6e1f6985914398559c6951d23cfc09b80fc4abf2
morghull <morghull@gmail.com>
2017-04-24 19:41:37 +0300

    some changes

--- m75d8/m75d8/m75d8.cs
+++ m75d8/m75d8/m75d8.cs
@@ -14,29 +14,40 @@ namespace m75d8
         public m75d8()
         {
             InitializeComponent();
+            var directoryToSearchIn = "d:\\rasch";
+            if (System.IO.Directory.Exists(directoryToSearchIn))
+                _textBox_mm75030Path.Text = System.IO.Directory.GetFiles(directoryToSearchIn, "mm75030.dbf", System.IO.SearchOption.AllDirectories).FirstOrDefault();
+
+            #region _toolStripButton_Exit Click
             _toolStripButton_Exit.Click += (s, e) =>
-            {
-                if (ISLibrary.msg.Show("Желаете завершить работу с программой?", ISLibrary.msg.DialogButtons.YesNo, ISLibrary.msg.DialogImage.Question) == DialogResult.Yes) Application.Exit();
-            };
+                {
+                    if (ISLibrary.msg.Show("Желаете завершить работу с программой?", ISLibrary.msg.DialogButtons.YesNo, ISLibrary.msg.DialogImage.Question) == DialogResult.Yes) Application.Exit();
+                };
+            #endregion
+            #region _toolStripButton_Ok Click
             _toolStripButton_Ok.Click += (s, e) =>
-            {
-                ISLibrary.msg.Show("Ошибка при работе с SQL-сервером!", "Ошибка", ISLibrary.msg.DialogButtons.Ok, ISLibrary.msg.DialogImage.Error);
-            };
+                {
+                    ISLibrary.msg.Show("Ошибка при работе с SQL-сервером!", "Ошибка", ISLibrary.msg.DialogButtons.Ok, ISLibrary.msg.DialogImage.Error);
+                };
+            #endregion
+            #region _radioButton_NcInclude Click
             _radioButton_NcInclude.Click += (s, e) =>
-            {
-                var dt = DatabaseManager.doQuery(new Npgsql.NpgsqlCommand("select nceh,nc,kd from vd.tabceh order by nceh;"));
-                var result = ISLibrary.select.Show(dt);
-                if (result.Rows.Count == 0) _radioButton_NcAll.Checked = true;
-            };
+                {
+                    var dt = DatabaseManager.doQuery(new Npgsql.NpgsqlCommand("select nceh,nc,kd from vd.tabceh order by nceh;"));
+                    var result = ISLibrary.select.Show(dt);
+                    if (result.Rows.Count == 0) _radioButton_NcAll.Checked = true;
+                };
+            #endregion
+            #region _btnGetPathTo_mm75030 Click
             _btnGetPathTo_mm75030.Click += (s, e) =>
-            {
-                OpenFileDialog fd = new OpenFileDialog();
-                fd.Filter = "mm75030|mm75030.dbf";
-                fd.InitialDirectory = Application.StartupPath;
-                var result = fd.ShowDialog();
-                if (result == System.Windows.Forms.DialogResult.OK) _textBox_mm75030Path.Text = fd.FileName;
-            };
-            //System.IO.Directory.GetFiles("","");
+                {
+                    OpenFileDialog fd = new OpenFileDialog();
+                    fd.Filter = "mm75030|mm75030.dbf";
+                    fd.InitialDirectory = Application.StartupPath;
+                    var result = fd.ShowDialog();
+                    if (result == System.Windows.Forms.DialogResult.OK) _textBox_mm75030Path.Text = fd.FileName;
+                };
+            #endregion
         }
     }
 }
