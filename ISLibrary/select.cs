cb207455a5f4c2bb9e1c720f15a6bf5d09e4542b
morghull <morghull@gmail.com>
2017-04-26 16:47:23 +0300

    add changes in select form

--- ISLibrary/select.cs
+++ ISLibrary/select.cs
@@ -14,9 +14,23 @@ namespace ISLibrary
         public select()
         {
             InitializeComponent();
-            _toolStripButton_Cancel.Click += (s, e) => 
+            _toolStripButton_Cancel.Click += (s, e) =>
             {
+                ((DataTable)_dataGridView_Right.DataSource).RejectChanges();
                 this.Close();
+            };
+            _toolStripButton_SingleRight.Click += (s, e) =>
+            {
+                DataTable dtRight = (DataTable)_dataGridView_Right.DataSource;
+                DataTable dtLeft = (DataTable)_dataGridView_Left.DataSource;
+                foreach (DataGridViewRow row in _dataGridView_Left.SelectedRows)
+                {
+                    dtRight.ImportRow(((DataRowView)row.DataBoundItem).Row);
+                    dtRight.Rows[dtRight.Rows.Count - 1].SetAdded();
+                    ((DataRowView)row.DataBoundItem).Row.Delete();
+                }
+                //dtRight.AcceptChanges();
+                //dtLeft.AcceptChanges();
             };
         }
         public static DataTable Show(DataTable dtSource)
@@ -25,11 +39,13 @@ namespace ISLibrary
             var dt_right = dtSource.Clone();
             sel._dataGridView_Left.DataSource = dtSource;
             sel._dataGridView_Right.DataSource = dt_right;
+            //sel._dataGridView_Left.Sort(sel._dataGridView_Left.Columns[0], ListSortDirection.Ascending);
+            //sel._dataGridView_Right.Sort(sel._dataGridView_Right.Columns[0], ListSortDirection.Ascending);
             var maximumLengthForColumns =
                 Enumerable.Range(0, dtSource.Columns.Count)
                 .Select(col => dtSource.AsEnumerable()
                                      .Select(row => row[col]).OfType<string>()
-                                     .Max(val => val.Length )).ToList();
+                                     .Max(val => val.Length)).ToList();
             var maxColumnIndex = maximumLengthForColumns.IndexOf(maximumLengthForColumns.Max());
             //sel._dataGridView_Left.Columns.GetLastColumn(DataGridViewElementStates.Visible, DataGridViewElementStates.None).AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill;
             sel._dataGridView_Left.Columns[maxColumnIndex].AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill;
