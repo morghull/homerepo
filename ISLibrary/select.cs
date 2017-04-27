74765eb95f96db98309a641ac11b4ac5b24ca963
morghull <morghull@gmail.com>
2017-04-27 16:33:32 +0300

    add changes in select form

--- ISLibrary/select.cs
+++ ISLibrary/select.cs
@@ -18,18 +18,20 @@ namespace ISLibrary
             _dataGridView_Right.DoubleBuffered(true);
             _toolStripButton_Cancel.Click += (s, e) =>
             {
+                ((DataTable)_dataGridView_Left.DataSource).RejectChanges();
                 ((DataTable)_dataGridView_Right.DataSource).RejectChanges();
                 this.Close();
             };
             _toolStripButton_SingleRight.Click += (s, e) =>
             {
-                DataTable dtRight = (DataTable)_dataGridView_Right.DataSource;
                 DataTable dtLeft = (DataTable)_dataGridView_Left.DataSource;
+                DataTable dtRight = (DataTable)_dataGridView_Right.DataSource;
                 foreach (DataGridViewRow row in _dataGridView_Left.SelectedRows)
                 {
-                    dtRight.ImportRow(((DataRowView)row.DataBoundItem).Row);
-                    dtRight.Rows[dtRight.Rows.Count - 1].SetAdded();
-                    ((DataRowView)row.DataBoundItem).Row.Delete();
+                    DataRow dr = ((DataRowView)row.DataBoundItem).Row;
+                    dr.SetAdded();
+                    dtRight.ImportRow(dr);
+                    dr.Delete();
                 }
                 //dtRight.AcceptChanges();
                 //dtLeft.AcceptChanges();
@@ -41,17 +43,16 @@ namespace ISLibrary
             var dt_right = dtSource.Clone();
             sel._dataGridView_Left.DataSource = dtSource;
             sel._dataGridView_Right.DataSource = dt_right;
-            //sel._dataGridView_Left.Sort(sel._dataGridView_Left.Columns[0], ListSortDirection.Ascending);
-            //sel._dataGridView_Right.Sort(sel._dataGridView_Right.Columns[0], ListSortDirection.Ascending);
+            sel._dataGridView_Left.Sort(sel._dataGridView_Left.Columns[0], ListSortDirection.Ascending);
+            sel._dataGridView_Right.Sort(sel._dataGridView_Right.Columns[0], ListSortDirection.Ascending);
             var maximumLengthForColumns =
                 Enumerable.Range(0, dtSource.Columns.Count)
                 .Select(col => dtSource.AsEnumerable()
                                      .Select(row => row[col]).OfType<string>()
                                      .Max(val => val.Length)).ToList();
             var maxColumnIndex = maximumLengthForColumns.IndexOf(maximumLengthForColumns.Max());
-            //sel._dataGridView_Left.Columns.GetLastColumn(DataGridViewElementStates.Visible, DataGridViewElementStates.None).AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill;
-            sel._dataGridView_Left.Columns[maxColumnIndex].AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill;
-            sel._dataGridView_Right.Columns[maxColumnIndex].AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill;
+            sel._dataGridView_Left.Columns[(maximumLengthForColumns.Max() > 15) ? maxColumnIndex : sel._dataGridView_Left.Columns.Count - 1].AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill;
+            sel._dataGridView_Right.Columns[(maximumLengthForColumns.Max() > 15) ? maxColumnIndex : sel._dataGridView_Right.Columns.Count - 1].AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill;
             sel.ShowDialog();
             return dt_right;
         }
