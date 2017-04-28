cb2c704be2b0bfe1194eb01cbbc8ef66e22f399c
morghull <morghull@gmail.com>
2017-04-28 11:40:18 +0300

    add changes with graphics, trying to draw rectangle with X button

--- DynamicMenu/DynamicMenu/frmWellcome.Designer.cs
+++ DynamicMenu/DynamicMenu/frmWellcome.Designer.cs
@@ -59,7 +59,7 @@
             // 
             this.splitContainer1.Panel2.Controls.Add(this.testTabControl);
             this.splitContainer1.Size = new System.Drawing.Size(1122, 555);
-            this.splitContainer1.SplitterDistance = 533;
+            this.splitContainer1.SplitterDistance = 242;
             this.splitContainer1.TabIndex = 2;
             // 
             // testTabControl
@@ -74,14 +74,14 @@
             this.testTabControl.Location = new System.Drawing.Point(0, 0);
             this.testTabControl.Name = "testTabControl";
             this.testTabControl.SelectedIndex = 0;
-            this.testTabControl.Size = new System.Drawing.Size(585, 555);
+            this.testTabControl.Size = new System.Drawing.Size(876, 555);
             this.testTabControl.TabIndex = 1;
             // 
             // tabPage1
             // 
             this.tabPage1.Location = new System.Drawing.Point(4, 25);
             this.tabPage1.Name = "tabPage1";
-            this.tabPage1.Size = new System.Drawing.Size(577, 526);
+            this.tabPage1.Size = new System.Drawing.Size(868, 526);
             this.tabPage1.TabIndex = 4;
             this.tabPage1.Text = "tabPage1";
             this.tabPage1.UseVisualStyleBackColor = true;
@@ -91,7 +91,7 @@
             this.tabPage2.Location = new System.Drawing.Point(4, 25);
             this.tabPage2.Name = "tabPage2";
             this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage2.Size = new System.Drawing.Size(570, 501);
+            this.tabPage2.Size = new System.Drawing.Size(577, 526);
             this.tabPage2.TabIndex = 1;
             this.tabPage2.Text = "tabPage2";
             this.tabPage2.UseVisualStyleBackColor = true;
@@ -100,7 +100,7 @@
             // 
             this.tabPage3.Location = new System.Drawing.Point(4, 25);
             this.tabPage3.Name = "tabPage3";
-            this.tabPage3.Size = new System.Drawing.Size(570, 501);
+            this.tabPage3.Size = new System.Drawing.Size(577, 526);
             this.tabPage3.TabIndex = 2;
             this.tabPage3.Text = "tabPage3";
             this.tabPage3.UseVisualStyleBackColor = true;
@@ -109,19 +109,19 @@
             // 
             this.tabPage4.Location = new System.Drawing.Point(4, 25);
             this.tabPage4.Name = "tabPage4";
-            this.tabPage4.Size = new System.Drawing.Size(570, 501);
+            this.tabPage4.Size = new System.Drawing.Size(577, 526);
             this.tabPage4.TabIndex = 3;
             this.tabPage4.Text = "tabPage4";
             this.tabPage4.UseVisualStyleBackColor = true;
             // 
-            // Form1
+            // frmWellcome
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1122, 577);
             this.Controls.Add(this.splitContainer1);
             this.Controls.Add(this.statusStrip1);
-            this.Name = "Form1";
+            this.Name = "frmWellcome";
             this.Text = "Form1";
             this.splitContainer1.Panel2.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
--- DynamicMenu/DynamicMenu/frmWellcome.cs
+++ DynamicMenu/DynamicMenu/frmWellcome.cs
@@ -12,6 +12,9 @@ namespace DynamicMenu
 {
     public partial class frmWellcome : Form
     {
+        private const int Xwid = 8;
+        private const int tab_margin = 5;
+        private Graphics g;
         public frmWellcome()
         {
             InitializeComponent();
@@ -22,10 +25,46 @@ namespace DynamicMenu
             testTabControl.SizeMode = TabSizeMode.Fixed;
             testTabControl.DrawItem += (s, e) =>
             {
-                //This code will render a "x" mark at the end of the Tab caption. 
-                e.Graphics.DrawString("x", e.Font, Brushes.Gray, e.Bounds.Right - 15, e.Bounds.Top + 4);
+                //This code will render a "x" mark at the end of the Tab caption.
+                g = e.Graphics;
+                e.Graphics.DrawString("x", e.Font, Brushes.Gray, e.Bounds.Right - 17, e.Bounds.Top + 3);
                 e.Graphics.DrawString(this.testTabControl.TabPages[e.Index].Text, e.Font, Brushes.Black, e.Bounds.Left + 12, e.Bounds.Top + 4);
+                e.Graphics.DrawRectangle(new Pen(Brushes.Gray), e.Bounds.Right - 18, e.Bounds.Top + 6, 12, 12);
                 e.DrawFocusRectangle();
+
+
+                //Brush box_brush;
+                //Pen box_pen = (Pen)Pens.Gray.Clone();
+                //box_pen.Width = 1.8F;
+                //box_brush = Brushes.Silver;
+                //box_pen = Pens.Gray;
+
+                //Rectangle tab_rect = this.testTabControl.GetTabRect(e.Index);
+                //RectangleF layout_rect = new RectangleF(
+                //                tab_rect.Left + tab_margin,
+                //                tab_rect.Top + tab_margin,
+                //                tab_rect.Width - 2 * tab_margin,
+                //                tab_rect.Height - 2 * tab_margin);
+                //e.Graphics.FillRectangle(box_brush,
+                //    layout_rect.Right - Xwid,
+                //    layout_rect.Top,
+                //    Xwid,
+                //    Xwid);
+                //e.Graphics.DrawRectangle(box_pen,
+                //    e.Bounds.Right - 18,
+                //    e.Bounds.Top + 6,
+                //    12,
+                //    12);
+                //e.Graphics.DrawLine(box_pen,
+                //    e.Bounds.Right - 14,
+                //    e.Bounds.Top + 7,
+                //    e.Bounds.Right - 7,
+                //    e.Bounds.Top + 14);
+                //e.Graphics.DrawLine(box_pen,
+                //    layout_rect.Right - Xwid,
+                //    layout_rect.Top + Xwid,
+                //    layout_rect.Right,
+                //    layout_rect.Top);
             };
             testTabControl.MouseDown += (s, e) =>
             {
@@ -39,9 +78,24 @@ namespace DynamicMenu
                     {
                         //if (MessageBox.Show("Would you like to Close this Tab?", "Confirm", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
                         //{
-                            this.testTabControl.TabPages.RemoveAt(i);
-                            break;
-                       // }
+                        this.testTabControl.TabPages.RemoveAt(i);
+                        break;
+                        // }
+                    }
+                }
+            };
+            testTabControl.MouseHover += (s, e) =>
+            {
+                var pos = this.testTabControl.PointToClient(Cursor.Position);
+                for (int i = 0; i < this.testTabControl.TabPages.Count; i++)
+                {
+                    Rectangle r = testTabControl.GetTabRect(i);
+                    //Getting the position of the "x" mark.
+                    Rectangle closeButton = new Rectangle(r.Right - 15, r.Top + 4, 15, 15);
+                    if (closeButton.Contains(pos))
+                    {
+                        var g = this.testTabControl.TabPages[i].CreateGraphics();
+                        g.DrawRectangle(new Pen(Brushes.Red), this.testTabControl.TabPages[i].Bounds.Right - 18, this.testTabControl.TabPages[i].Bounds.Top + 6, 12, 12);
                     }
                 }
             };
